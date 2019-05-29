500e72344272a9e6f684f59526e641fb91c06c0a
Aleksandr Serov <sashaserov2010@gmail.com>
2019-05-29 21:37:40 +0300

    update

--- tests/unit/weather/components/WeatherContainer.spec.js
+++ tests/unit/weather/components/WeatherContainer.spec.js
@@ -1,8 +1,11 @@
 import Vuex from "vuex";
 import WeatherContainer from "@/components/WeatherContainer";
 import { shallowMount, createLocalVue } from "@vue/test-utils";
+import chai from "chai";
 import { expect } from "chai";
 import sinon from "sinon";
+import sinonChai from "sinon-chai";
+chai.use(sinonChai);
 
 describe("WeatherContainer.vue", () => {
   let wrapper;
@@ -17,7 +20,7 @@ describe("WeatherContainer.vue", () => {
 
     getters = {
       location: () => "New York",
-      weatherDescription: () => "Loght Cloud",
+      weatherDescription: () => "Light Cloud",
       imageAbbr: () => "lc.png",
       weatherTemp: () => -10.0,
       loading: () => loading
@@ -29,10 +32,45 @@ describe("WeatherContainer.vue", () => {
       getters,
       actions
     });
+    wrapper = shallowMount(WeatherContainer, {
+      localVue,
+      store
+    });
   };
 
-  wrapper = shallowMount(WeatherContainer, {
-    localVue,
-    store
+  it("should render the correct content when the app is loading", () => {
+    setUpWrapper(true);
+    expect(wrapper.html()).to.contain('<div class="loader"></div>');
+    expect(wrapper.html()).to.not.contain(
+      '<h1 class="subtitle weather__city">New York</h1>'
+    );
+    expect(wrapper.html()).to.not.contain(
+      '<p class="weather__description">Light Cloud</p>'
+    );
+    expect(wrapper.html()).to.not.contain(
+      '<p class="weather__temperature">-10 ºC</p></div>'
+    );
+  });
+  it("should render the correct content when the app is not loading", () => {
+    setUpWrapper(false);
+    expect(wrapper.html()).to.contain(
+      '<h1 class="subtitle weather__city">New York</h1>'
+    );
+    expect(wrapper.html()).to.contain(
+      '<p class="weather__description">Light Cloud</p>'
+    );
+    expect(wrapper.html()).to.contain(
+      '<p class="weather__temperature">-10 ºC</p></div>'
+    );
+    expect(wrapper.html()).to.not.contain('<div class="loader"></div>');
+  });
+  it('should call the "fetchWeather" action once when created', () => {
+    setUpWrapper(false);
+    expect(actions.fetchWeather).to.have.been.calledOnce;
+  });
+  it('should also call the "fetchWeather" action when "id" is changed', () => {
+    setUpWrapper(false);
+    wrapper.setData({ id: "1398823" });
+    expect(actions.fetchWeather).to.have.been.calledTwice;
   });
 });
