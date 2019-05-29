0cafd0c37a63fe84dce0560a95a39e862ebdd088
Aleksandr Serov <sashaserov2010@gmail.com>
2019-05-29 17:02:03 +0300

    update

--- tests/unit/weather/components/WeatherContainer.spec.js
+++ tests/unit/weather/components/WeatherContainer.spec.js
@@ -1,3 +1,38 @@
-describe('WeatherContainer.vue', () => {
-  
+import Vuex from "vuex";
+import WeatherContainer from "@/components/WeatherContainer";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import { expect } from "chai";
+import sinon from "sinon";
+
+describe("WeatherContainer.vue", () => {
+  let wrapper;
+  let getters;
+  let actions;
+  let store;
+  let localVue;
+
+  const setUpWrapper = loading => {
+    localVue = createLocalVue();
+    localVue.use(Vuex);
+
+    getters = {
+      location: () => "New York",
+      weatherDescription: () => "Loght Cloud",
+      imageAbbr: () => "lc.png",
+      weatherTemp: () => -10.0,
+      loading: () => loading
+    };
+    const actions = {
+      fetchWeather: sinon.stub()
+    };
+    store = new Vuex.Store({
+      getters,
+      actions
+    });
+  };
+
+  wrapper = shallowMount(WeatherContainer, {
+    localVue,
+    store
+  });
 });
