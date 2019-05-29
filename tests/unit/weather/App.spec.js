39364e54f8ba69c35808496012a43ebf59afdeb1
Aleksandr Serov <sashaserov2010@gmail.com>
2019-05-29 09:40:35 +0300

    update

--- tests/unit/weather/App.spec.js
+++ tests/unit/weather/App.spec.js
@@ -17,7 +17,6 @@ describe("App.vue", () => {
         return loading;
       }
     };
-
     store = new Vuex.Store({
       getters
     });
@@ -29,19 +28,20 @@ describe("App.vue", () => {
     });
   };
 
-  it("should diplay the current day's date", () => {
+  it("should display the current day's date", () => {
     setUpWrapper(false);
+
     const formattedDate = new Date().toDateString();
     expect(wrapper.html()).to.contain(formattedDate);
   });
 
-  it("should displat rhe foorer links when app is not loading", () => {
+  it("should display the footer links when application is not loading", () => {
     setUpWrapper(false);
     const footerLinks = wrapper.find(".app__cities");
+
     expect(footerLinks.html()).to.contain(
       '<router-link-stub to="/weather/2459115">New York City, New York</router-link-stub>'
     );
-
     expect(footerLinks.html()).to.contain(
       '<router-link-stub to="/weather/468739">Buenos Aires, Argentina</router-link-stub>'
     );
@@ -61,25 +61,22 @@ describe("App.vue", () => {
 
   it("should not display footer links when application is loading", () => {
     setUpWrapper(true);
-    const footerLinks = wrapper.find(".app__cities");
-    console.log(footerLinks.html());
-
-    expect(footerLinks.html()).to.not.contain(
+    expect(wrapper.html()).to.not.contain(
       '<router-link-stub to="/weather/2459115">New York City, New York</router-link-stub>'
     );
-    expect(footerLinks.html()).to.not.contain(
+    expect(wrapper.html()).to.not.contain(
       '<router-link-stub to="/weather/468739">Buenos Aires, Argentina</router-link-stub>'
     );
-    expect(footerLinks.html()).to.not.contain(
+    expect(wrapper.html()).to.not.contain(
       '<router-link-stub to="/weather/2122265">Moscow, Russia</router-link-stub>'
     );
-    expect(footerLinks.html()).to.not.contain(
+    expect(wrapper.html()).to.not.contain(
       '<router-link-stub to="/weather/1118370">Tokyo, Japan</router-link-stub>'
     );
-    expect(footerLinks.html()).to.not.contain(
+    expect(wrapper.html()).to.not.contain(
       '<router-link-stub to="/weather/1105779">Sydney, Australia</router-link-stub>'
     );
-    expect(footerLinks.html()).to.not.contain(
+    expect(wrapper.html()).to.not.contain(
       '<router-link-stub to="/weather/1398823">Lagos, Nigeria</router-link-stub>'
     );
   });
